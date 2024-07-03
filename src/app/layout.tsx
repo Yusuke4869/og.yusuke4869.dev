1fdfbe562215794e84cb174f8e7da9d8b3f1b4f0
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2024-07-04 01:13:20 +0900

    🎨 update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,9 @@
+import type { Metadata } from "next";
 import type { FC, ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "OG Image Generator - Yusuke4869",
+};
 
 interface Props {
   children: ReactNode;
@@ -6,6 +11,9 @@ interface Props {
 
 const Layout: FC<Props> = ({ children }) => (
   <html lang="ja">
+    <head>
+      <link href="/favicon.ico" rel="icon" />
+    </head>
     <body>
       <main>{children}</main>
     </body>
